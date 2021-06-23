a8c49a0c251a9e056e97b8faad51c55b2c9b116d
Jordan Roessle <screamaimfire@me.com>
2021-06-23 14:14:51 -0700

    bonus complete, used switch statement

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -8,10 +8,10 @@ var form = d3.select("#form");
 function grabFilters () {
     var filters = [];
     filters.push(d3.select("#datetime").property("value"));
-    filters.push(d3.select("#city").property("value"));
-    filters.push(d3.select("#state").property("value"));
-    filters.push(d3.select("#country").property("value"));
-    filters.push(d3.select("#shape").property("value"));
+    filters.push(d3.select("#city").property("value").toLowerCase());
+    filters.push(d3.select("#state").property("value").toLowerCase());
+    filters.push(d3.select("#country").property("value").toLowerCase());
+    filters.push(d3.select("#shape").property("value").toLowerCase());
     return filters;
 }
 
@@ -23,24 +23,46 @@ function formChange() {
     // grab inserted date
     var inputValues = grabFilters();
     
-    console.log(inputValues)
-
     // filter data based on date given 
-    // var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    var filteredData = tableData; 
+    inputValues.forEach((value, index) => {
+        if(value !== "") {
+            // switch to do the correct filter
+            switch(index) {
+                case 0:
+                    filteredData = filteredData.filter(sighting => sighting.datetime === value)
+                    break;
+                case 1:
+                    filteredData = filteredData.filter(sighting => sighting.city === value)
+                    break;
+                case 2:
+                    filteredData = filteredData.filter(sighting => sighting.state === value)
+                    break;
+                case 3:
+                    filteredData = filteredData.filter(sighting => sighting.country === value)
+                    break;
+                case 4:
+                    filteredData = filteredData.filter(sighting => sighting.shape === value)
+                    break;
+                default:
+                    console.log("Something went very wrong!!!");
+            }
+        }
+    });
 
-    // // grab table body
-    // var tbody = d3.select("tbody");
+    // grab table body
+    var tbody = d3.select("tbody");
 
-    // // empty body 
-    // tbody.html("");
+    // empty body 
+    tbody.html("");
 
-    // // insert table 
-    // filteredData.forEach(sighting => {
-    //     var row = tbody.append("tr");
-    //     Object.values(sighting).forEach(value => {
-    //         row.append("td").text(value);
-    //     });
-    // });
+    // insert table 
+    filteredData.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.values(sighting).forEach(value => {
+            row.append("td").text(value);
+        });
+    });
 }
 
 // call function on event change 
