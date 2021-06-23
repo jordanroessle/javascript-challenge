6fee12b630ceb630ac01257c6eee226d65c048a5
Jordan Roessle <screamaimfire@me.com>
2021-06-23 13:59:35 -0700

    button removed in order to allow form to be submit by pressing enter, replace with submit input, filters correctly grabbed

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,9 +1,19 @@
 // from data.js
 var tableData = data;
 
-// select button and form
-var button = d3.select("#filter-btn"); 
+// select button and form 
 var form = d3.select("#form");
+
+// function to grab all inserted filters 
+function grabFilters () {
+    var filters = [];
+    filters.push(d3.select("#datetime").property("value"));
+    filters.push(d3.select("#city").property("value"));
+    filters.push(d3.select("#state").property("value"));
+    filters.push(d3.select("#country").property("value"));
+    filters.push(d3.select("#shape").property("value"));
+    return filters;
+}
 
 // function on event change
 function formChange() {
@@ -11,28 +21,28 @@ function formChange() {
     d3.event.preventDefault();
 
     // grab inserted date
-    var inputElement = d3.select(".form-control");
-    var inputValue = inputElement.property("value");
+    var inputValues = grabFilters();
     
+    console.log(inputValues)
+
     // filter data based on date given 
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
-    // grab table body
-    var tbody = d3.select("tbody");
+    // // grab table body
+    // var tbody = d3.select("tbody");
 
-    // empty body 
-    tbody.html("");
+    // // empty body 
+    // tbody.html("");
 
-    // insert table 
-    filteredData.forEach(sighting => {
-        var row = tbody.append("tr");
-        Object.values(sighting).forEach(value => {
-            row.append("td").text(value);
-        });
-    });
+    // // insert table 
+    // filteredData.forEach(sighting => {
+    //     var row = tbody.append("tr");
+    //     Object.values(sighting).forEach(value => {
+    //         row.append("td").text(value);
+    //     });
+    // });
 }
 
 // call function on event change 
-button.on("click", formChange);
 form.on("submit", formChange);
 
